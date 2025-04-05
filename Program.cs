b5d7e6757af63b66b11e2e26bf49f7dce70b8f2c
ELJ0RG3 <Jorge.ocana@edu.uag.mx>
2025-04-05 17:57:18 -0600

    Update Program.cs
    Los estudiantes deberán desarrollar un programa en C# que implemente uno de los siguientes algoritmos en un grafo dirigido y ponderado:
    
    Dijkstra (camino mínimo desde un nodo origen a todos los demás).
    Floyd (todas las parejas de nodos).
    Warshall (busca si hay caminos).
    El programa debe permitir al usuario:
    
    Definir un grafo ingresando nodos y aristas con pesos.
    Ejecutar el algoritmo seleccionado y mostrar los resultados de manera clara.
    Visualizar la representación de la matriz de adyacencia y/o las rutas encontradas.
    Instrucciones
    Implementa en C# una estructura de grafo dirigido y ponderado utilizando listas de adyacencia o matrices de adyacencia.
    
    Desarrolla una opción en el programa para ejecutar uno de los algoritmos mencionados.
    
    Muestra los resultados de manera legible, ya sea en consola o mediante una interfaz gráfica opcional.
    
    Entrega el proyecto en un repositorio de GitHub.
    
    El README.md debe incluir:
    
    Descripción del proyecto y objetivos.
    Instrucciones de instalación y uso.
    Ejemplos de entrada y salida esperada.
    Explicación del algoritmo implementado y su complejidad.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,142 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Implementación_de_Algoritmos_de_Caminos_Mínimos_en_Grafos_
+class Grafo
 {
-    class Program
+    private int[,] matrizAdyacencia;
+    private int numNodos;
+    private const int INF = int.MaxValue;
+
+    public Grafo(int n)
     {
-        static void Main(string[] args)
+        numNodos = n;
+        matrizAdyacencia = new int[n, n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                matrizAdyacencia[i, j] = (i == j) ? 0 : INF;
+    }
+
+    public void AgregarArista(int origen, int destino, int peso)
+    {
+        if (origen >= 0 && origen < numNodos && destino >= 0 && destino < numNodos)
+            matrizAdyacencia[origen, destino] = peso;
+    }
+
+    public void MostrarMatriz()
+    {
+        Console.WriteLine("Matriz de Adyacencia:");
+        for (int i = 0; i < numNodos; i++)
         {
+            for (int j = 0; j < numNodos; j++)
+            {
+                if (matrizAdyacencia[i, j] == INF)
+                    Console.Write("INF".PadLeft(5));
+                else
+                    Console.Write(matrizAdyacencia[i, j].ToString().PadLeft(5));
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void Dijkstra(int origen)
+    {
+        int[] distancias = new int[numNodos];
+        bool[] visitado = new bool[numNodos];
+
+        for (int i = 0; i < numNodos; i++)
+        {
+            distancias[i] = INF;
+            visitado[i] = false;
+        }
+
+        distancias[origen] = 0;
+
+        for (int contador = 0; contador < numNodos - 1; contador++)
+        {
+            int u = MinDistancia(distancias, visitado);
+            if (u == -1) break;
+            visitado[u] = true;
+
+            for (int v = 0; v < numNodos; v++)
+            {
+                if (!visitado[v] && matrizAdyacencia[u, v] != INF && distancias[u] != INF &&
+                    distancias[u] + matrizAdyacencia[u, v] < distancias[v])
+                {
+                    distancias[v] = distancias[u] + matrizAdyacencia[u, v];
+                }
+            }
+        }
+
+        Console.WriteLine("\nDistancias mínimas desde el nodo " + origen + ":");
+        for (int i = 0; i < numNodos; i++)
+        {
+            if (distancias[i] == INF)
+                Console.WriteLine($"Nodo {i}: INF");
+            else
+                Console.WriteLine($"Nodo {i}: {distancias[i]}");
+        }
+    }
+
+    private int MinDistancia(int[] distancias, bool[] visitado)
+    {
+        int min = INF, minIndex = -1;
+        for (int v = 0; v < numNodos; v++)
+        {
+            if (!visitado[v] && distancias[v] <= min)
+            {
+                min = distancias[v];
+                minIndex = v;
+            }
+        }
+        return minIndex;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el número de nodos: ");
+        int n = int.Parse(Console.ReadLine());
+        Grafo grafo = new Grafo(n);
+
+        while (true)
+        {
+            Console.WriteLine("\n1. Agregar arista");
+            Console.WriteLine("2. Mostrar matriz de adyacencia");
+            Console.WriteLine("3. Ejecutar Dijkstra");
+            Console.WriteLine("4. Salir");
+            Console.Write("Seleccione una opción: ");
+            int opcion = int.Parse(Console.ReadLine());
+
+            if (opcion == 1)
+            {
+                Console.Write("Ingrese nodo origen: ");
+                int origen = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese nodo destino: ");
+                int destino = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese peso: ");
+                int peso = int.Parse(Console.ReadLine());
+                grafo.AgregarArista(origen, destino, peso);
+            }
+            else if (opcion == 2)
+            {
+                grafo.MostrarMatriz();
+            }
+            else if (opcion == 3)
+            {
+                Console.Write("Ingrese nodo origen para Dijkstra: ");
+                int origen = int.Parse(Console.ReadLine());
+                grafo.Dijkstra(origen);
+            }
+            else if (opcion == 4)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Opción inválida.");
+            }
         }
     }
 }
